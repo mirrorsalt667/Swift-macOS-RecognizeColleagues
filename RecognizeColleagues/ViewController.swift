3b894980f11ba9461f8fedc9453a258b17da75d2
StephenMBAM1 <jjjjllears@icloud.com>
2022-10-29 13:22:19 +0800

    Initial Commit

---
+++ RecognizeColleagues/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RecognizeColleagues
+//
+//  Created by Stephen on 2022/10/29.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
